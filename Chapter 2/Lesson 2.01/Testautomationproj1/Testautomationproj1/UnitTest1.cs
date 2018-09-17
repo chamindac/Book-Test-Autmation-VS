d77eb2b418c97565d11826c2ccc9ad374c0b00d2
PushpaH <33891436+PushpaH@users.noreply.github.com>
2018-09-17 19:53:46 +0530

    Add solution files

---
+++ Chapter 2/Lesson 2.01/Testautomationproj1/Testautomationproj1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Testautomationproj1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("--start-maximized");
+            IWebDriver driver = new ChromeDriver(option);           
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl("https://www.google.lk/");
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("lst-ib")));
+            IWebElement textField = driver.FindElement(By.Id("lst-ib"));
+            textField.SendKeys("Selenium");
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("input[value='Google Search'][class='lsb']")));
+            IWebElement searchButton = driver.FindElement(By.CssSelector("input[value='Google Search']"));
+            searchButton.Click();
+            Assert.AreEqual(true, wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains("Selenium - Google Search")));
+            driver.Dispose();
+        }
+    }
+}
